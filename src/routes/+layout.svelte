3a8273ffd8784368befb9be76043a127485c6811
Tim Rooke <timrooke1991@gmail.com>
2024-03-13 11:00:25 +0000

    Scroll to top btn

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script>
 	import '$lib/assets/scss/global.scss';
 	import { page } from '$app/stores';
+	import { fly } from 'svelte/transition';
+
 	import throttle from 'just-throttle';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
@@ -21,13 +23,9 @@
 		showFooter = $page.url.pathname !== '/';
 	});
 
-	const handleScroll = throttle(() => {
-		// Early return if we're above mobile width
-		if (window.outerWidth >= 768) {
-			if ($isScrollingDown) $isScrollingDown = false;
-			return;
-		}
+	let lastScrollPosition = 0;
 
+	const handleScroll = throttle(() => {
 		const currentScrollPosition = window.scrollY;
 		const delta = lastScrollPosition - currentScrollPosition;
 		if (delta > 0 && delta < 10) {
@@ -41,6 +39,10 @@
 		}
 		lastScrollPosition = currentScrollPosition;
 	}, 100);
+
+	function scrollToTop() {
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	}
 </script>
 
 <svelte:window on:scroll={handleScroll} />
@@ -53,6 +55,15 @@
 	</PageTransition>
 </main>
 
+{#if $isScrollingDown}
+	<button
+		on:click={scrollToTop}
+		class="back-to-top"
+		in:fly={{ x: 300, duration: 500 }}
+		out:fly={{ x: 300, duration: 500 }}>&#8679</button
+	>
+{/if}
+
 {#if showFooter}
 	<Footer />
 {/if}
@@ -60,5 +71,31 @@
 <style lang="scss">
 	main {
 		padding-bottom: calc(4 * var(--halfNote));
+	}
+
+	.back-to-top {
+		position: fixed;
+		display: flex;
+		align-items: center;
+		justify-content: space-around;
+		box-sizing: border-box;
+		bottom: 20px;
+		right: 20px;
+		padding: 10px 20px;
+		font-size: 1em;
+		height: 50px;
+		width: 50px;
+		border-radius: 25px;
+		color: var(--purple);
+		background-color: var(--grey);
+		border: 2px solid var(--purple);
+		cursor: pointer;
+		transition: opacity 0.3s ease;
+		z-index: 1000;
+	}
+
+	/* Hide button when not scrolling down */
+	.back-to-top:hover {
+		opacity: 0.7;
 	}
 </style>
